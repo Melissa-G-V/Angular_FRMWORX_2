40b50e9af2c7056c5446d1eb2f97b4927f80df1a
Melissa Gomes Victor <melissagomesvictor@gmail.com>
2024-04-07 15:28:36 -0300

    fixed gitignore

---
+++ dist/nasa/browser/chunk-GOI2NOER.js
@@ -0,0 +1,1 @@
+import"./chunk-JPE6FXMD.js";
---
+++ dist/nasa/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-24MALJKX.mjs";import"./chunk-SGIPORXP.mjs";import"./chunk-QOJAWCCK.mjs";import"./chunk-3SZVCSKE.mjs";import"./chunk-H6KHSOBK.mjs";export{a as default};
